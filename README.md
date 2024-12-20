997dc190c6c44b74cddb6608fd7b1ac3ce448415
Dmytro Ivasenko <dmtivasenko@gmail.com>
2024-12-20 16:26:52 +0300

    add todo app

--- README.md
+++ README.md
@@ -1,9 +1,50 @@
-# React TypeScript Starter Pack
+# Todo App
 
-To use this template click `Use this template`
+## Description
+A dynamic and user-friendly todo application developed with React. It allows users to add, edit, delete, and filter tasks, with real-time updates and smooth interactions. The app is integrated with an external API to manage tasks. The clean, modern interface is designed for efficiency, offering a pleasant task management experience.
 
-### Available Scripts
+## Technologies used
+* HTML5
+* CSS3
+* SASS
+* Bulma
+* JS (ES6+)
+* React
+* TypeScript
+* Rest API
+* Git
 
-`Deploy` - available to deploy your application to gh-pages
+## Instructions for Working with the Project
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+	1. Open your terminal and run the following command to clone the repository:
+
+        git clone https://github.com/modeltoIT/todo-app.git
+
+
+	2. Move into the project directory:
+    
+        cd todo-app
+
+
+	3. Ensure that Node.js version v20.x.x is installed. Verify this by running:
+        
+        node -v
+        
+        If the version is incorrect, install or update Node.js from Node.js official website.
+
+	4. Install the necessary project dependencies by running:
+        
+        npm install
+
+
+	5. Start the development server with the command:
+
+        npm start
+
+## View Project
+
+* Live Demo: [View the Project](https://modeltoit.github.io/todo-app/)
+
+## Preview
+
+![Preview](./src/images/preview.png)
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,136 @@
-import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { UserWarning } from './UserWarning';
+import { changeTodo, getTodos, USER_ID } from './api/todos';
+import { Todo } from './types/Todo';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { ErrorMessage } from './components/ErrorMessage';
+import { TodoFilter } from './types/TodoFilter';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [filterBy, setFilterBy] = useState<TodoFilter>('all');
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [idsToDelete, setIdsToDelete] = useState<number[]>([]);
+  const [isHiddenError, setIsHiddenError] = useState(true);
+  const [idsForStatusChange, setIdsForStatusChange] = useState<number[]>([]);
+  const timeoutRef = useRef<number | null>(null);
+
+  const removeErrorMessage = () => {
+    setIsHiddenError(true);
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = null;
+    }
+
+    timeoutRef.current = window.setTimeout(() => {
+      setErrorMessage('');
+    }, 1000);
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(prevTodo => prevTodo.filter(todo => todo.id !== id));
+  };
+
+  const errorMessageHandler = useCallback((er: Error) => {
+    setErrorMessage(er.message ?? String(er));
+    setIsHiddenError(false);
+
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = window.setTimeout(() => {
+      removeErrorMessage();
+    }, 3000);
+  }, []);
+
+  const handleUpdate = (updatedTodo: Todo): Promise<void> => {
+    return changeTodo(updatedTodo)
+      .then(result => {
+        setTodos(prevTodo =>
+          prevTodo.map(todo => {
+            if (updatedTodo.id === todo.id) {
+              return result;
+            }
+
+            return todo;
+          }),
+        );
+      })
+      .catch((error: Error) => {
+        errorMessageHandler(error);
+
+        throw new Error('');
+      });
+  };
+
+  useEffect(() => {
+    getTodos()
+      .then(serverTodos => {
+        setTodos(serverTodos);
+      })
+      .catch(errorMessageHandler);
+  }, []);
+
+  const filteredTodos = () => {
+    let filteredTD = todos;
+
+    if (filterBy !== 'all') {
+      filteredTD = todos.filter(todo =>
+        filterBy === 'active' ? !todo.completed : todo.completed,
+      );
+    }
+
+    return filteredTD;
+  };
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header
+          todos={todos}
+          onError={errorMessageHandler}
+          onSuccess={setTodos}
+          setTempTodo={setTempTodo}
+          errorMessage={errorMessage}
+          setIdsForStatusChange={setIdsForStatusChange}
+        />
+
+        <TodoList
+          renderedList={filteredTodos()}
+          tempTodo={tempTodo}
+          idsToDelete={idsToDelete}
+          resetIdsToDelete={setIdsToDelete}
+          onError={errorMessageHandler}
+          handleDelete={handleDelete}
+          handleUpdate={handleUpdate}
+          idsForStatusChange={idsForStatusChange}
+        />
+
+        {(tempTodo || !!todos.length) && (
+          <Footer
+            filterBy={filterBy}
+            setFilter={setFilterBy}
+            todos={todos}
+            onClearCompleted={setIdsToDelete}
+          />
+        )}
+      </div>
+
+      <ErrorMessage
+        errorMessage={errorMessage}
+        removeError={removeErrorMessage}
+        isHiddenError={isHiddenError}
+      />
     </div>
   );
 };
